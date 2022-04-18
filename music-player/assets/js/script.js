4e3b358ff5d5755024b77ab98e8da9c2e642d8e5
Favour Afolayan <favourafolayan@gmail.com>
2022-04-19 00:39:26 +0100

    :rocket: add music-player repository

---
+++ music-player/assets/js/script.js
@@ -0,0 +1,65 @@
+const button = document.getElementsByTagName('button')[0];
+const pauseIcon = document.getElementsByClassName('pause-icon')[0];
+const playIcon = document.getElementsByClassName('play-icon')[0];
+const bars = document.getElementsByClassName('bar');
+
+const song = new Audio('assets/mp3/asa-eyo.mp3');
+
+let interval = null;
+
+const playSong = () => {
+  // song.play();
+
+  playIcon.classList.add('paused');
+
+  pauseIcon.classList.remove('paused');
+
+  showBars();
+
+  button.removeEventListener('click', playSong);
+  button.addEventListener('click', pauseSong);
+};
+
+const pauseSong = () => {
+  // song.pause();
+
+  hideBars();
+
+  playIcon.classList.remove('paused');
+
+  pauseIcon.classList.add('paused');
+
+  button.removeEventListener('click', pauseSong);
+  button.addEventListener('click', playSong);
+};
+
+const showBars = () => {
+  interval = setInterval(() => {
+    for (let i = 0; i < bars.length; i++) {
+      const randomNumber = Math.floor(Math.random() * 5) + 1;
+      // number between 1 * 5
+
+      const height = randomNumber * 10;
+      // we are multiplying the random number by 10 to get the height of the bar
+
+      bars[i].style.height = `${height}px`;
+    }
+  }, 200);
+};
+
+const hideBars = () => {
+  // we need to clear the interval so that the bars don't keep popping up
+  clearInterval(interval);
+
+  for (let i = 0; i < bars.length; i++) {
+    bars[i].style.height = '0px';
+  }
+};
+
+button.addEventListener('click', () => {
+  if (pauseIcon.classList.contains('paused')) {
+    playSong();
+  } else {
+    pauseSong();
+  }
+});
