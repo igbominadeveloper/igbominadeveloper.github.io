c102549f470dd0f48627ed09803ff853d3483579
Favour Afolayan <favourafolayan@gmail.com>
2022-04-27 23:47:35 +0100

    :sparkles: add delete and constrain adding

--- wordle/index.js
+++ wordle/index.js
@@ -12,10 +12,50 @@ document.addEventListener('alpine:init', () => {
     },
 
     handleKeyPress(event) {
-      //   debugger;
-      // validate key here
+      if (!this.validKeyPressed(event)) return;
 
-      this.board[this.currentRowIndex][this.currentTileIndex] = event.key;
+      this.fillTile(event.key);
+    },
+
+    validKeyPressed(event) {
+      const { key } = event;
+
+      if (key.length === 1 && key.match(/[a-z]/i)) {
+        return true;
+      }
+
+      if (key === 'Enter') {
+        // check if the word is complete and match it with the next random word
+      }
+
+      if (key === 'Backspace') {
+        if (this.currentRowIndex === 0 && this.currentTileIndex === 0)
+          return false;
+
+        if (this.currentTileIndex === 0) {
+          this.currentRowIndex--;
+
+          this.currentTileIndex = this.board[this.currentRowIndex].length - 1;
+
+          this.board[this.currentRowIndex][this.currentTileIndex] = '';
+
+          return false;
+        }
+
+        this.currentTileIndex--;
+
+        this.board[this.currentRowIndex][this.currentTileIndex] = '';
+
+        return false;
+      }
+
+      return false;
+    },
+
+    fillTile(key) {
+      if (this.currentRowIndex === this.guessesAllowed) return;
+
+      this.board[this.currentRowIndex][this.currentTileIndex] = key;
 
       if (this.currentTileIndex === this.wordLength - 1) {
         this.currentTileIndex = 0;
