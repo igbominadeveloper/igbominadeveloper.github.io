8786fa1a6dee2aa6f0760001a063a625822dc74b
Favour Afolayan <favourafolayan@gmail.com>
2024-01-07 01:41:26 +0100

    Add rotate button

--- html-js-slider/index.js
+++ html-js-slider/index.js
@@ -1,4 +1,6 @@
 const slides = document.querySelectorAll('.slide');
+const buttons = document.querySelectorAll('button');
+const slider = document.querySelector('.slider');
 
 const expandItem = (event) => {
   resetFlexBasis();
@@ -13,6 +15,21 @@ const resetFlexBasis = () => {
   slides.forEach((slide) => (slide.style.flexBasis = ''));
 };
 
+const changeOrientation = (event) => {
+  const orientation = event.currentTarget.textContent;
+  console.log(orientation);
+
+  if (orientation === 'portrait') {
+    slider.style.transform = 'rotate(90deg)';
+  } else {
+    slider.style.transform = 'rotate(0deg)';
+  }
+};
+
 slides.forEach((slide) => {
   slide.addEventListener('click', expandItem);
 });
+
+buttons.forEach((button) =>
+  button.addEventListener('click', changeOrientation)
+);
