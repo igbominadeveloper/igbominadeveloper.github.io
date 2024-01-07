578b86d5b884b7aa7b952e12478ff79a8f964d05
Favour Afolayan <favourafolayan@gmail.com>
2024-01-07 02:35:45 +0100

    Add mode toggle buttons

--- html-js-slider/index.js
+++ html-js-slider/index.js
@@ -1,6 +1,19 @@
 const slides = document.querySelectorAll('.slide');
-const buttons = document.querySelectorAll('button');
+const orientationButtons = document.querySelectorAll('.orientation button');
 const slider = document.querySelector('.slider');
+const colorModeButton = document.querySelector('.theme-toggle button');
+const lightTheme = document.querySelector('.theme-toggle button .sun');
+const darkTheme = document.querySelector('.theme-toggle button .moon');
+const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+
+if (prefersDarkScheme) {
+  document.body.classList.toggle('dark-mode');
+}
+
+if (document.body.classList.contains('dark-mode')) {
+  darkTheme.style.display = 'none';
+  lightTheme.style.display = 'block';
+}
 
 const expandItem = (event) => {
   resetFlexBasis();
@@ -26,10 +39,37 @@ const changeOrientation = (event) => {
   }
 };
 
+const toggleTheme = () => {
+  const darkMode = !!document.body.classList.contains('dark-mode');
+
+  if (prefersDarkScheme.matches) {
+    if (darkMode) {
+      document.body.classList.toggle('light-mode');
+
+      darkTheme.style.display = 'block';
+      lightTheme.style.display = 'none';
+    } else {
+      document.body.classList.toggle('dark-mode');
+      darkTheme.style.display = 'none';
+      lightTheme.style.display = 'block';
+    }
+  }
+
+  //   document.body.classList.remove('dark-mode');
+  //   document.body.classList.add('light-mode');
+  // } else {
+  //   document.body.classList.remove('light-mode');
+  //   document.body.classList.add('dark-mode');
+  // }
+};
+
+// register event listeners
 slides.forEach((slide) => {
   slide.addEventListener('click', expandItem);
 });
 
-buttons.forEach((button) =>
+orientationButtons.forEach((button) =>
   button.addEventListener('click', changeOrientation)
 );
+
+colorModeButton.addEventListener('click', toggleTheme);
